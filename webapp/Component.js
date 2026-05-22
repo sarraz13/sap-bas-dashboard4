b3fcb3b4b7a3783c92d9eecc6b0684e35fa7ce7e
Sarra Zekri <sarahzekri13@gmail.com>
2026-05-22 19:44:07 +0000

    Initial commit

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("com.iliade.dash4.dash4.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
